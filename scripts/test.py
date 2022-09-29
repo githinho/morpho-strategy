90a5bb27e02d9cf3a3b6a9c5ec87b4f341d11af0
Eugen Druzin <edruzin@gmail.com>
2022-09-29 11:08:39 +0200

    feat: add actions test flow

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,14 @@
 from brownie import Contract
 
+
 def main():
     dai = Contract.from_explorer("0x6b175474e89094c44da98b954eedeac495271d0f")
-    balance = dai.balanceOf(
-        "0x6b175474e89094c44da98b954eedeac495271d0f"
-    )
+    balance = dai.balanceOf("0x6b175474e89094c44da98b954eedeac495271d0f")
     print(balance)
 
-    row = "{};\n".format(
-        balance
-    )
+    row = "{},0\n".format(balance)
 
     # Append-adds at last
-    dataFile = open("./data/data.csv", "a")  # append mode
+    dataFile = open("./data/liquidity.csv", "a")  # append mode
     dataFile.write(row)
     dataFile.close()

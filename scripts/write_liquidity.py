e482a48f462bccba5aed1f2dad4e0f34b33bef27
Eugen Druzin <edruzin@gmail.com>
2022-09-29 12:13:36 +0200

    chore: update liquidity data collection flow

--- scripts/write_liquidity.py
+++ scripts/write_liquidity.py
@@ -1,16 +1,27 @@
 from brownie import Contract
 
+
 def main():
+    # TODO: set strategy contract address
     strategy_address = "0x"
-    # it would be nice to remove Contract.from_address and import MorphoStrategy class but brownie cannot import abstract class
-    strategy = Contract.from_address(strategy_address)
-    timestamp = chain.time() # or use chain.height for block number
-    (strategyBalanceOnPool, strategyBalanceInP2P, strategyTotalBalance) = strategy.getStrategySupplyBalance()
-    (p2pSupplyAmount, p2pBorrowAmount, poolSupplyAmount, poolBorrowAmount) = strategy.getCurrentMarketLiquidity()
+    # it would be nice to remove Contract.from_explorer and import MorphoStrategy class but brownie cannot import abstract class
+    strategy = Contract.from_explorer(strategy_address)
+    timestamp = chain.time()  # or use chain.height for block number
+    (
+        strategyBalanceOnPool,
+        strategyBalanceInP2P,
+        strategyTotalBalance,
+    ) = strategy.getStrategySupplyBalance()
+    (
+        p2pSupplyAmount,
+        p2pBorrowAmount,
+        poolSupplyAmount,
+        poolBorrowAmount,
+    ) = strategy.getCurrentMarketLiquidity()
     maxP2PSupply = strategy.getMaxP2PSupply()
 
     # create row in CSV table
-    row = "{};{};{};{};{};{};{};{};{}\n".format(
+    row = "{},{},{},{},{},{},{},{},{}\n".format(
         timestamp,
         strategyTotalBalance,
         strategyBalanceInP2P,
@@ -19,10 +30,10 @@ def main():
         p2pBorrowAmount,
         poolSupplyAmount,
         poolBorrowAmount,
-        maxP2PSupply
+        maxP2PSupply,
     )
 
     # append mode
-    dataFile = open("./data/liquidity.csv", "a")  
+    dataFile = open("./data/liquidity.csv", "a")
     dataFile.write(row)
     dataFile.close()

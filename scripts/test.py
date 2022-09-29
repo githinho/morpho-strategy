45fd2f260a11daa88b502b8163829ecd94bc1f54
Eugen Druzin <edruzin@gmail.com>
2022-09-29 14:38:32 +0200

    chore: update actions

--- scripts/test.py
+++ scripts/test.py
@@ -4,13 +4,26 @@ from brownie import chain
 
 
 def main():
-    strategyAddress = getEnvVariable("STRATEGY_ADDRESS")
-    dai = Contract.from_explorer(strategyAddress)
-    balance = dai.balanceOf(strategyAddress)
+    os.chdir("data/")
+    addresses = getEnvVariable("STRATEGY_ADDRESSES")
+    for address in addresses.split(','):
+        fetchAndStoreLiquidityForStrategy(address.strip())
+
+
+def getEnvVariable(key):
+    try:
+        return os.environ[key]
+    except KeyError:
+        print("ERROR: Please set the environment variable:", key)
+        exit(1)
+
+
+def fetchAndStoreLiquidityForStrategy(strategyAddress):
+    strategy = Contract.from_explorer(strategyAddress)
+    balance = strategy.balanceOf(strategyAddress)
     timestamp = chain.time()
     row = "{},{},0,0,0,0,0,0,99\n".format(timestamp, balance)
 
-    os.chdir("data/")
     fileName = "strategy_" + strategyAddress + ".csv"
     print("Writing liquidity data to file:", fileName)
     if os.path.isfile(fileName):
@@ -25,10 +38,3 @@ def main():
         dataFile.write("Timestamp,Strategy Total Balance,Strategy Balance in P2P,Strategy Balance On Pool,Market P2P Supply,Market P2P Borrow,Market Pool Supply,Market Pool Borrow,Max P2P Supply\n")
         dataFile.write(row)
         dataFile.close()
-
-def getEnvVariable(key):
-    try:
-        return os.environ[key]
-    except KeyError:
-        print("ERROR: Please set the environment variable:", key)
-        exit(1)
